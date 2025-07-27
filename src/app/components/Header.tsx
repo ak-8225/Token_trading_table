e911983e6c9d63c536adfa384cd31651c9d64c1e
ak-8225 <ak8225@srmist.edu.in>
2025-07-27 19:43:00 +0530

    logo bottom trapezoid matches triangle base

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -17,8 +17,8 @@ export default function Header() {
             {/* Logo and Brand */}
             <div className="flex items-center space-x-1 sm:space-x-2 flex-shrink-0 select-none">
               <svg width="32" height="32" viewBox="0 0 32 32" className="mr-4" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <polygon points="16,10 24,18 8,18" fill="white" />
-                <polygon points="10,22 22,22 16,28" fill="white" />
+                <polygon points="16,8 24,18 8,18" fill="white" />
+                <polygon points="6,24 26,24 24,20 8,20" fill="white" />
               </svg>
               <span className="font-bold text-white text-[28px] tracking-tight" style={{letterSpacing:'-1px',fontFamily:'inherit'}}>AXIOM</span>
               <span className="text-white text-[24px] font-normal ml-3" style={{fontFamily:'inherit'}}>Pro</span>
