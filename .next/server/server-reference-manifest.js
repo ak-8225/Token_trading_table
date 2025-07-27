ae99e79caaed9334b7fcb26d8318cd1ab7497021
ak-8225 <ak8225@srmist.edu.in>
2025-07-27 20:03:28 +0530

    fix: build errors

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"mH8H5g6G5+tKp5YWOYX1DpLjqdYJAOo+C+mE3ccV63E=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
