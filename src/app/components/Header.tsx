cb0061566b76455acde01a3b675c9409c1f7177e
ak-8225 <ak8225@srmist.edu.in>
2025-07-27 19:50:15 +0530

    chore: increase bottom part of logo for emphasis

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
             <div className="flex items-center space-x-1 sm:space-x-2 flex-shrink-0 select-none">
               <svg width="40" height="40" viewBox="0 0 32 32" className="mr-4" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <polygon points="16,8 24,18 8,18" fill="white" />
-                <polygon points="6,24 26,24 24,20 8,20" fill="white" />
+                <polygon points="5,28 27,28 25,20 7,20" fill="white" />
               </svg>
               <span className="font-bold text-white text-[28px] tracking-tight" style={{letterSpacing:'-1px',fontFamily:'inherit'}}>AXIOM</span>
               <span className="text-white text-[24px] font-normal ml-3" style={{fontFamily:'inherit'}}>Pro</span>
